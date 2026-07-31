1eeeffe8ad33eeba3e9359048c250a4ded4e6b7c
25nilesh <nileshkumar4276@gmail.com>
2026-07-31 21:14:50 +0530

    Time: 4 ms (86.59%) | Memory: 25.3 MB (84.45%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -7,19 +7,23 @@ public:
             freq[word[i]-'a']++;
         }
         sort(freq.begin(),freq.end(),greater<int>());
-        n=freq.size();
         int result=0;
-        for(int i=0;i<n;i++){
-            int count=freq[i];
-            if(i<8){
-                result+=count;
-            }else if(i>=8 && i<16){
-                result+=2*count;
-            }else if(i>=16 && i<24){
-                result+=3*count;
-            }else{
-                result+=4*count;
-            }
+        // n=freq.size();
+        // for(int i=0;i<n;i++){
+        //     int count=freq[i];
+            // if(i<8){
+            //     result+=count;
+            // }else if(i>=8 && i<16){
+            //     result+=2*count;
+            // }else if(i>=16 && i<24){
+            //     result+=3*count;
+            // }else{
+            //     result+=4*count;
+            // }
+            
+        // }
+        for (int i = 0; i < 26; i++) {
+            result += freq[i] * (i / 8 + 1);
         }
         return result;
     }
