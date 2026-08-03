87b83d5fbb23f99cc319c950654253758a8eade1
25nilesh <nileshkumar4276@gmail.com>
2026-08-03 21:04:20 +0530

    Time: 27 ms (64.47%) | Memory: 136.8 MB (60.61%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int dp[50001];
-    int solve(vector<int>&nums,int i,int n){
+    int n;
+    int solve(vector<int>&nums,int i){
         if(i>=n) return 0;
         if(dp[i]!=-1) return dp[i];
         int result=INT_MIN;
 
-        result=max(result,nums[i]-solve(nums,i+1,n));
+        result=max(result,nums[i]-solve(nums,i+1));
 
-        if(i+1<n) result=max(result,nums[i]+nums[i+1]-solve(nums,i+2,n));
+        if(i+1<n) result=max(result,nums[i]+nums[i+1]-solve(nums,i+2));
 
-        if(i+2<n) result=max(result,nums[i]+nums[i+1]+nums[i+2]-solve(nums,i+3,n));
+        if(i+2<n) result=max(result,nums[i]+nums[i+1]+nums[i+2]-solve(nums,i+3));
 
         return dp[i]=result;
     }
     string stoneGameIII(vector<int>& stoneValue) {
-        int n=stoneValue.size();
+        n=stoneValue.size();
         memset(dp,-1,sizeof dp);
-        if(solve(stoneValue,0,n)>0) return "Alice";
-        if(solve(stoneValue,0,n)<0) return "Bob";
+        int diff=solve(stoneValue,0);
+        if(diff>0) return "Alice";
+        if(diff<0) return "Bob";
         return "Tie";
     }
 };
