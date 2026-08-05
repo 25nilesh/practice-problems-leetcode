ba53489dbe3134837c4ef9949df8936b1f9b0237
25nilesh <nileshkumar4276@gmail.com>
2026-08-05 13:40:33 +0530

    Time: 196 ms (57.93%) | Memory: 305.8 MB (68.97%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public: 
-    void dfs(vector<vector<int>> &graph,int node,vector<bool>& visited,vector<bool>& suspicious){
-        visited[node]=true;
+    void dfs(vector<vector<int>> &graph,int node,vector<bool>& suspicious){
         suspicious[node]=true;
         for(auto &ngbr:graph[node]){
-            if(!visited[ngbr]){
-                dfs(graph,ngbr,visited,suspicious);
+            if(!suspicious[ngbr]){
+                dfs(graph,ngbr,suspicious);
             }
         }
     }
@@ -17,9 +16,8 @@ public:
             int v=edge[1];
             graph[u].push_back(v);
         }
-        vector<bool> visited(n,false);
         vector<bool> suspicious(n,false);
-        dfs(graph,k,visited,suspicious);
+        dfs(graph,k,suspicious);
         
         for(auto& edge: invocations){
             int u=edge[0];
