18e48380bfd1e9d9189e0a415ace31233627f68a
25nilesh <nileshkumar4276@gmail.com>
2026-08-26 12:44:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (45.09%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        // brute force 
+        // brute force  t.c = O(n^3)
+        // int n=s.size();
+        // for(int len=k;len<=n;len++){
+        //     string res="";
+        //     for(int j=0;j<=n-len;j++){
+        //         string temp=s.substr(j,len);
+        //         int countOnes=0;
+        //         for(char& ch:temp){
+        //             countOnes+=(ch=='1')?1:0;
+        //         }
+        //         if(countOnes==k){
+        //             if(res.empty() || temp<res){
+        //                 res=temp;
+        //             }
+        //         }
+        //     }
+        //     if(!res.empty()) return res;
+        // }
+        // return "";
+
+        // optimal using sliding window 
         int n=s.size();
-        for(int len=k;len<=n;len++){
-            string res="";
-            for(int j=0;j<=n-len;j++){
-                string temp=s.substr(j,len);
-                int countOnes=0;
-                for(char& ch:temp){
-                    countOnes+=(ch=='1')?1:0;
+        int i=0;
+        int j=0;
+        int cntOnes=0;
+        string result="";
+        while(j<n){
+            if(s[j]=='1'){
+                cntOnes++;
+            }
+            while(cntOnes>k || s[i]=='0'){
+                if(s[i]=='1'){
+                    cntOnes--;
                 }
-                if(countOnes==k){
-                    if(res.empty() || temp<res){
-                        res=temp;
-                    }
+                i++;
+            }
+            if(cntOnes==k){
+                int len=j-i+1;
+                string temp=s.substr(i,len);
+                if(result.empty() || result.size()>temp.size() || (result.size()==temp.size() && temp<result) ){
+                    result=temp;
                 }
             }
-            if(!res.empty()) return res;
+            j++;
         }
-        return "";
+        return result;
+
     }
 };
