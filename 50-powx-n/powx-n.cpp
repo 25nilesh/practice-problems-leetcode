09f64b0e90bc2bf31ff00c50deac345ab4136296
25nilesh <nileshkumar4276@gmail.com>
2026-08-27 11:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.56%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -15,7 +15,22 @@ public:
         return res;
     }
     double myPow(double x, int n) {
-        // if(n<0) return binaryExponentationRec(x,n);
-        return binaryExponentationRec(x,n);
+        // return binaryExponentationRec(x,n);
+
+        // binary exponentation iterative 
+        long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        double ans=1;
+        while(N>0){
+            if(N%2==1){
+                ans=ans*x;
+            }
+            x=x*x;
+            N/=2;
+        }
+        return ans;
     }
 };
