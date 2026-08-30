23e59ee6ae21a273933eb13dbc9c6e8843fa9519
25nilesh <nileshkumar4276@gmail.com>
2026-08-30 16:34:56 +0530

    Time: 360 ms (32.48%) | Memory: 234.1 MB (27.40%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        int n = nums.size();
+        int n=nums.size();
+        vector<int> vec=nums;
+        sort(vec.begin(),vec.end());
 
-        vector<pair<int, int>> arr;
+        int groupNum=0;
+        unordered_map<int,int> numToGroup;
+        numToGroup[vec[0]]=groupNum;
 
-        for (int i = 0; i < n; i++) {
-            arr.push_back({nums[i], i});
+        unordered_map<int,list<int>> groupToList;
+        groupToList[groupNum].push_back(vec[0]);
+
+        for(int i=1;i<n;i++){
+            if(abs(vec[i]-vec[i-1])>limit){
+                groupNum+=1;
+            }
+            numToGroup[vec[i]]=groupNum;
+            groupToList[groupNum].push_back(vec[i]);
         }
-
-        sort(arr.begin(), arr.end());
-
-        vector<int> ans = nums;
-
-        int start = 0;
-
-        while (start < n) {
-            int end = start;
-
-            // Find one connected group
-            while (end + 1 < n &&
-                   arr[end + 1].first - arr[end].first <= limit) {
-                end++;
-            }
-
-            vector<int> values;
-            vector<int> indices;
-
-            for (int i = start; i <= end; i++) {
-                values.push_back(arr[i].first);
-                indices.push_back(arr[i].second);
-            }
-
-            sort(indices.begin(), indices.end());
-
-            for (int i = 0; i < values.size(); i++) {
-                ans[indices[i]] = values[i];
-            }
-
-            start = end + 1;
+        // build the answer,merge the groups 
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int group=numToGroup[num];
+            // smallest available number in the group 
+            nums[i]=*(groupToList[group].begin());
+            groupToList[group].pop_front();
         }
-
-        return ans;
+        return nums;
     }
 };
