730757f70732224c2f1cb4c1b0ce11cf69bc1cb0
25nilesh <nileshkumar4276@gmail.com>
2026-09-04 11:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (85.58%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<int> prefixMax(n);
-        prefixMax[0]=nums[0];
-        vector<int> prefixMin(n);
-        prefixMin[n-1]=nums[n-1];
-        for(int i=1;i<n;i++){
-            prefixMax[i]=max(prefixMax[i-1],nums[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            prefixMin[i]=min(prefixMin[i+1],nums[i]);
-        }
+        int largest=nums[0];
+        int mini=*min_element(nums.begin(),nums.end());
+        // vector<int> suffixMin(n);
+        // suffixMin[n-1]=nums[n-1];
+        // for(int i=n-2;i>=0;i--){
+        //     suffixMin[i]=min(suffixMin[i+1],nums[i]);
+        // }
         int idx=-1;
         for(int i=0;i<n;i++){
-            if(prefixMax[i]-prefixMin[i]<=k) {
+            largest=max(largest,nums[i]);
+            if(largest-mini<=k) {
                 idx=i;
                 break;
+            }
+            if(nums[i]==mini && i+1<n) {
+                mini=*min_element(nums.begin()+i+1,nums.end());
             }
         }
         return idx;
