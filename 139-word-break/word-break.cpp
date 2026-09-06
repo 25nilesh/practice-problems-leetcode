1e383bf67129453560cc3988af39256c76dc703c
25nilesh <nileshkumar4276@gmail.com>
2026-09-06 14:24:27 +0530

    Time: 18 ms (15.54%) | Memory: 21.3 MB (7.75%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int n;
     int dp[301];
-    bool solve(int idx,string s,unordered_set<string>& st){
+    bool solve(int idx,string& s,unordered_set<string>& st){
         if(idx==n){
             return true;
+        }
+        if(dp[idx]!=-1) {
+            return dp[idx];
         }
         if(st.find(s)!=st.end()) {
             return true;
         }
-        if(dp[idx]!=-1) return dp[idx];
         for(int l=1;l<=n;l++){
             string str=s.substr(idx,l);
             if(st.find(str)!=st.end() && solve(idx+l,s,st)){
@@ -22,7 +24,7 @@ public:
         memset(dp,-1,sizeof(dp));
         n=s.size();
         unordered_set<string> st;
-        for(auto str:wordDict){
+        for(auto& str:wordDict){
             st.insert(str);
         }
         return solve(0,s,st);
