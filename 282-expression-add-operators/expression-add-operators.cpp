e39bcd8d412cb6a2aed976951317ec9436a84286
25nilesh <nileshkumar4276@gmail.com>
2026-09-11 16:47:48 +0530

    Time: 419 ms (34.76%) | Memory: 94.1 MB (7.91%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
+    #define ll long long
     vector<string> result;
-    void solve(string& num,long long target,int idx,string exp,long long value,long long prev){
+    void solve(string& num,int target,int idx,string exp,ll value,ll prev){
         if(idx==num.size()){
             if(value==target){
                 result.push_back(exp);
@@ -9,20 +10,21 @@ public:
             return;
         }
         for(int j=idx;j<num.size();j++){
-            string part=num.substr(idx,j-idx+1);
-            long long curr=stoll(part);
             if(j>idx && num[idx]=='0') continue;
+            string part=num.substr(idx,j-idx+1);
+            ll curr=stoll(part);
             if(idx==0){
                 solve(num,target,j+1,part,curr,curr);
             }else{
                 solve(num,target,j+1,exp+"+"+part,value+curr,curr);
                 solve(num,target,j+1,exp+"-"+part,value-curr,-curr);
-                solve(num,target,j+1,exp+"*"+part,value-prev+prev*curr,prev*curr);
+                solve(num,target,j+1,exp+"*"+part,(value-prev)+(prev*curr),prev*curr);
             }
         }
     }
     vector<string> addOperators(string num, int target) {
-        solve(num,target,0,"",0,0);
+        string str="";
+        solve(num,target,0,str,0,0);
         return result;
     }
 };
