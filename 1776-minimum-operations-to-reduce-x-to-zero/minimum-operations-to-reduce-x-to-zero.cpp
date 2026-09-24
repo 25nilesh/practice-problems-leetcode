3f0af7e0c0d891055e3d1ec417d471a715b5e76e
25nilesh <nileshkumar4276@gmail.com>
2026-09-24 07:36:37 +0530

    Time: 207 ms (11.90%) | Memory: 172.4 MB (5.35%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -3,21 +3,19 @@ public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
         unordered_map<int,int> mp;
-        int ans=INT_MIN;
-        int sum=0;
         mp[0]=-1;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            mp[sum]=i;
+        mp[nums[0]]=0;
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+            mp[nums[i]]=i;
         }
-        if(sum<x) return -1;
-        int target=sum-x;
-        sum=0;
+        if(nums[n-1]<x) return -1;
+        int target=nums[n-1]-x;
+        int ans=INT_MIN;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-            int ele=sum-target;
-            if(mp.find(ele)!=mp.end()){
-                ans=max(ans,i-mp[ele]);
+            int remaining=nums[i]-target;
+            if(mp.find(remaining)!=mp.end()){
+                ans=max(ans,i-mp[remaining]);
             }
         }
         return ans==INT_MIN?-1:n-ans;
